fb92b279c3932f6bd9dc7629b493aa7768f4fc67
yyyyaaa <phatghaa@gmail.com>
2025-06-13 20:49:06 +0700

    fix: assets

--- packages/dashboard/hooks/contract/use-hyperweb-assets.ts
+++ packages/dashboard/hooks/contract/use-hyperweb-assets.ts
@@ -7,57 +7,57 @@ import { useHyperwebChain } from './use-hyperweb-chain';
 import { useRpcEndpoint } from './use-rpc-endpoint';
 
 export interface PrettyAsset {
-	logoUrl: string;
-	symbol: string;
-	prettyChainName: string;
-	displayAmount: string;
-	dollarValue: string;
-	denom: string;
-	amount: string;
+  logoUrl: string;
+  symbol: string;
+  prettyChainName: string;
+  displayAmount: string;
+  dollarValue: string;
+  denom: string;
+  amount: string;
 }
 
 export const useHyperwebAssets = () => {
-	const { data: rpcEndpoint } = useRpcEndpoint();
-	const { address, chain, assetList } = useHyperwebChain();
-	const { data: prices } = useHyperwebAssetsPrices();
+  const { data: rpcEndpoint } = useRpcEndpoint();
+  const { address, chain, assetList } = useHyperwebChain();
+  const { data: prices } = useHyperwebAssetsPrices();
 
-	return useQuery({
-		queryKey: ['useHyperwebAssets', address],
-		queryFn: async () => {
-			if (!rpcEndpoint || !address || !prices) return null;
+  return useQuery({
+    queryKey: ['useHyperwebAssets', address],
+    queryFn: async () => {
+      if (!rpcEndpoint || !address) return null;
 
-			const { balances } = await getAllBalances(rpcEndpoint, {
-				address,
-				resolveDenom: true,
-			});
+      const { balances } = await getAllBalances(rpcEndpoint, {
+        address,
+        resolveDenom: true,
+      });
 
-			const assets: PrettyAsset[] = balances
-				.filter((b) => assetList.assets.some((a) => a.base === b.denom))
-				.map(({ amount, denom }) => {
-					const asset = assetList.assets.find((a) => a.base === denom)!;
+      const assets: PrettyAsset[] = balances
+        .filter((b) => assetList.assets.some((a) => a.base === b.denom))
+        .map(({ amount, denom }) => {
+          const asset = assetList.assets.find((a) => a.base === denom)!;
 
-					const logoUrl = getLogoUrlFromAsset(asset);
-					const symbol = asset.symbol;
-					const prettyChainName = chain.prettyName || chain.chainName;
-					const displayAmount = calcDisplayAmount(amount, asset);
-					const dollarValue = calcDollarValue(amount, asset, prices);
+          const logoUrl = getLogoUrlFromAsset(asset);
+          const symbol = asset.symbol;
+          const prettyChainName = chain.prettyName || chain.chainName;
+          const displayAmount = calcDisplayAmount(amount, asset);
+          const dollarValue = prices ? calcDollarValue(amount, asset, prices) : '0.00';
 
-					return {
-						logoUrl,
-						symbol,
-						prettyChainName,
-						displayAmount,
-						dollarValue,
-						denom,
-						amount,
-					};
-				});
+          return {
+            logoUrl,
+            symbol,
+            prettyChainName,
+            displayAmount,
+            dollarValue,
+            denom,
+            amount,
+          };
+        });
 
-			return {
-				assets,
-				totalDollarValue: calcTotalDollarValue(assets),
-			};
-		},
-		enabled: !!rpcEndpoint && !!address && !!prices,
-	});
+      return {
+        assets,
+        totalDollarValue: prices ? calcTotalDollarValue(assets) : '0.00',
+      };
+    },
+    enabled: !!rpcEndpoint && !!address,
+  });
 };
