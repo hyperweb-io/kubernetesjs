c40337946c5d032cd0fce1f17a817633829a25fd
Anmol1696 <anmol1696@gmail.com>
2025-09-24 22:56:31 +0400

    update interwebjs, remove some excluded parts

--- packages/interwebjs/scripts/codegen.ts
+++ packages/interwebjs/scripts/codegen.ts
@@ -8,35 +8,35 @@ const options = getDefaultSchemaSDKOptions({
   clientName: 'InterwebClient',
   includeSwaggerUrl: true,
   exclude: [
-    '*.v1beta1.*',
-    '*.v2beta1.*',
-    'io.k8s.api.events.v1.EventSeries',
-    'io.k8s.api.events.v1.Event',
     '*autoscaling.v2*',
   ],
-  jsonpatch: [
-    {
-      op: 'remove',
-      path: '/definitions/io.k8s.apimachinery.pkg.util.intstr.IntOrString/type'
-    },
-    {
-      op: 'remove',
-      path: '/definitions/io.k8s.apimachinery.pkg.util.intstr.IntOrString/format'
-    },
-    {
-      op: 'add',
-      path: '/definitions/io.k8s.apimachinery.pkg.util.intstr.IntOrString/oneOf',
-      value: [
-        { type: 'string' },
-        { type: 'integer', format: 'int32' }
-      ]
-    }
-  ]
+  // jsonpatch: [
+  //   {
+  //     op: 'remove',
+  //     path: '/definitions/io.k8s.apimachinery.pkg.util.intstr.IntOrString/type'
+  //   },
+  //   {
+  //     op: 'remove',
+  //     path: '/definitions/io.k8s.apimachinery.pkg.util.intstr.IntOrString/format'
+  //   },
+  //   {
+  //     op: 'add',
+  //     path: '/definitions/io.k8s.apimachinery.pkg.util.intstr.IntOrString/oneOf',
+  //     value: [
+  //       { type: 'string' },
+  //       { type: 'integer', format: 'int32' }
+  //     ]
+  //   }
+  // ]
 });
 const code = generateOpenApiClient(
   {
     ...options,
     npmApiClient: './client',
+    opsIndex: {
+      enabled: true,
+      emptyGroupLabel: 'core',
+    },
     operationNamingStrategy: {
       // aliases: {
       //   listCoreV1PodForAllNamespaces: 'getPods',
@@ -46,15 +46,11 @@ const code = generateOpenApiClient(
       // },
     },
     paths: {
-      exclude: ['*v1beta1*', '*v2beta1*'],
+      //exclude: ['*v1beta1*', '*v2beta1*'],
       excludeRequests: ['head', 'options'],
       excludeTags: [
         'storage_v1beta1',
-        '*v1beta1',
-        '*v2beta1',
-        '*v1beta1*',
-        '*v2beta1*',
-        '*autoscaling*'
+        '*autoscaling*',
       ],
     },
     includeTypeComments: true,
@@ -71,18 +67,6 @@ const code = generateOpenApiClient(
           'ApiExtWebhookClientConfig',
         'io.k8s.api.admissionregistration.v1.ServiceReference':
           'AdmissionServiceReference',
-        'dev.knative.internal.networking.v1alpha1.Certificate':
-          'KnativeCertificate',
-        'dev.knative.internal.networking.v1alpha1.CertificateList':
-          'KnativeCertificateList',
-        'dev.knative.internal.networking.v1alpha1.Ingress':
-          'KnativeIngress',
-        'dev.knative.internal.networking.v1alpha1.IngressList':
-          'KnativeIngressList',
-        'dev.knative.serving.v1.Service':
-          'KnativeService',
-        'dev.knative.serving.v1.ServiceList':
-          'KnativeServiceList',
       },
     },
   },
